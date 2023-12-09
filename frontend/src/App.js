649cbe3df108369df6dc144feb7b180587e1d5d7
nicard <nicolas.ard@gmail.com>
2023-12-09 14:29:38 +0100

    Minor changes at frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
+
+  if(getCookie('jwt-token')){
+    return (
+    <div className="App">
+    <h2>You are logged</h2>
+    </div>
+    );
+  }
   return (
     <div className="App">
-    
+    <h2>Welcome to the demo app</h2>
+
     <GoogleLogin
       onSuccess={credentialResponse => {
+        document.cookie = 'jwt-token='+credentialResponse.credential;
         console.log(credentialResponse);
       }}
       onError={() => {
@@ -16,6 +26,12 @@ function App() {
     />
     </div>
   );
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
 export default App;
