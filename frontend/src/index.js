763a2d642ccf1653707cc03e2a4086833adce63a
nicard <nicolas.ard@gmail.com>
2023-12-09 14:08:42 +0100

    Starting to add google authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="129539746772-fjto05brsno322451sr2djv2oeus4jb8.apps.googleusercontent.com">
+        <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
