29fda7c98ba487f87ee4dd82e337949d9c0f4a2d
nicard <nicolas.ard@gmail.com>
2023-12-18 22:34:28 +0100

    Adding autogenrated stuff for the frontend

--- frontendv2/src/modules/MyAccount.tsx
+++ frontendv2/src/modules/MyAccount.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
 import Cookies from 'universal-cookie';
+import { UserControllerApi, UserControllerApiFactory } from './generated-api/api';
+import { Configuration } from './generated-api/configuration';
+import axios from 'axios';
 
 function MyAccount() {
   const cookies = new Cookies();
   cookies.set('mycookie','valor',{ path: '/' });
+  console.log(cookies.get("jwt-token"));
+// Set your JWT token in the headers
+const jwtToken = cookies.get("jwt-token"); // Replace with your actual JWT token
+axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
+  UserControllerApiFactory().getProfile();
   return (
 <div>
     <h3>My account</h3>
