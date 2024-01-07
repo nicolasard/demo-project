654feef274a964086433ed9d42f63fc0f6113f85
nicard <nicolas.ard@gmail.com>
2024-01-08 00:09:54 +0100

    Starting to work in my own jwt token

--- backend/src/main/java/ar/nic/demoproject/utils/PrincipalMapper.java
+++ backend/src/main/java/ar/nic/demoproject/utils/PrincipalMapper.java
@@ -4,26 +4,38 @@ import ar.nic.demoproject.db.model.Currency;
 import ar.nic.demoproject.db.model.UserProfile;
 import ar.nic.demoproject.db.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import io.jsonwebtoken.Jwts;
 
 import javax.crypto.SecretKey;
-import java.security.Principal;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.sql.Date;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAmount;
 
 @Component
 public class PrincipalMapper {
 
     final UserProfileRepository userProfileRepository;
 
+    final String keyStorePath;
+
+    final String keyStoreKey;
+
     @Autowired
-    public PrincipalMapper(UserProfileRepository userProfileRepository) {
+    public PrincipalMapper(final UserProfileRepository userProfileRepository,
+                           @Value("${jwt-token.keystore-path}") final String keyStorePath,
+                           @Value("${jwt-token.keystore-password}") final String keyStoreKey) {
         this.userProfileRepository = userProfileRepository;
+        this.keyStorePath = keyStorePath;
+        this.keyStoreKey = keyStoreKey;
     }
 
     public Mono<UserProfile> getUserProfile(Principal principal){
@@ -42,14 +54,27 @@ public class PrincipalMapper {
      * we can do this out of the JWT token from Facebook, Apple auth, etc.
      */
     public Mono<String> authenticate(){
-        final SecretKey key = Jwts.SIG.HS256.key().build(); // TODO: I need to generate a keypair and save in the configuration
+        //TODO: This is under construction.
+        //TODO: Don't load the keystore for each transaction.
+        Key key;
+        try {
+            final KeyStore keystore = KeyStore.getInstance("JKS");
+            try (FileInputStream fis = new FileInputStream(this.keyStorePath)) {
+                keystore.load(fis, this.keyStoreKey.toCharArray());
+                key = keystore.getKey(keystore.aliases().nextElement(),this.keyStoreKey.toCharArray());
+            }
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException |
+                 UnrecoverableKeyException e) {
+            throw new RuntimeException(e);
+        }
+        final SecretKey key2 = Jwts.SIG.HS256.key().build(); // TODO: I need to generate a keypair and save in the configuration
                                                             // this is only for test
         return Mono.just(Jwts.builder()
                         .subject("Test") //TODO: In future use the id of user.
                         .claim("email","nicolas.ard@gmail.com")
                         .claim("name","Nicolas Ardison")
                         .expiration(Date.from(Instant.now().plus(6, ChronoUnit.DAYS)))
-                        .signWith(key)
+                        .signWith(key2)
                         .compact());
     }
 }
