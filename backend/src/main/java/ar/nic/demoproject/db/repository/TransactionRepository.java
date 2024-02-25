b5b916f715964969ead43c605f25d875e0d9b4b7
Nicolas Ardison <nicolasard@users.noreply.github.com>
2024-02-25 16:32:29 +0100

    Fixing to 2 decimals the amount

--- backend/src/main/java/ar/nic/demoproject/db/repository/TransactionRepository.java
+++ backend/src/main/java/ar/nic/demoproject/db/repository/TransactionRepository.java
@@ -11,19 +11,19 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface TransactionRepository extends ReactiveCrudRepository<Transaction, Long> {
 
-    @Query("select * from transactions t where t.userInternalId = :userInternalId ORDER BY t.date DESC")
+    @Query("SELECT * FROM transactions t WHERE t.userInternalId = :userInternalId ORDER BY t.date DESC")
     Flux<Transaction> findAllByUserInternalId(Mono<Integer> userInternalId);
 
 
-    @Query("select * from transactions t where t.userInternalId = :userInternalId and MONTH(t.`date`)=:month and YEAR(t.`date`)=:year order by t.`date` desc")
+    @Query("SELECT * FROM transactions t WHERE t.userInternalId = :userInternalId AND MONTH(t.`date`)=:month AND YEAR(t.`date`)=:year ORDER BY t.`date` DESC")
     Flux<Transaction> findAllByUserInternalId(Mono<Integer> userInternalId, final int month, final int year);
 
-    @Query("select * from transactions t where t.userInternalId = :userInternalId and t.transactionId = :transactionId")
+    @Query("SELECT * FROM transactions t WHERE t.userInternalId = :userInternalId AND t.transactionId = :transactionId")
     Mono<Transaction> findByUserInternalIdAndTransactionId(Mono<Integer> userInternalId, Long transactionId);
 
-    @Query("select DAY(t.`date`) as day ,sum(amount) as total \n" +
-            "from transactions t where userInternalId = :userInternalId and MONTH(t.`date`)=:month and YEAR(t.`date`)=:year " +
-            "GROUP by DAY(t.`date`) order by DAY(t.`date`) asc ")
+    @Query("SELECT DAY(t.`date`) AS day ,ROUND(SUM(amount),2) AS total \n" +
+            "FROM transactions t WHERE userInternalId = :userInternalId AND MONTH(t.`date`)=:month AND YEAR(t.`date`)=:year " +
+            "GROUP BY DAY(t.`date`) ORDER BY DAY(t.`date`) asc ")
     Flux<TotalDay> findTotalPerDay(final Integer userInternalId, final int month,final  int year);
 }
 
