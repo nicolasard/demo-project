6a54e6b4b4f8a505040ac2103f0cea388ddc29a4
Nicolas Ardison <nicolas.ardisonfiserv.com@Nicolass-MacBook-Air.local>
2024-09-05 20:53:06 +0200

    Fixing some icons

--- frontendv2/src/modules/HomePayments.tsx
+++ frontendv2/src/modules/HomePayments.tsx
@@ -63,6 +63,17 @@ const HomePayments = () => {
     return new Date(date).toLocaleDateString(intl.locale,options);
   };
 
+  const getClassName = (category: any): string => {
+      switch (category) {
+      case 'Services':
+        return 'bi bi-bootstrap-reboot';
+      case 'Leisure':
+        return 'bi bi-cup-straw';
+      default:
+        return 'bi bi-question';
+    }
+  };
+
   return (
     <div>
 
@@ -86,18 +97,18 @@ const HomePayments = () => {
                   <div className="col-auto d-flex align-items-center">
                     <div className="p-0">
                               <div style={{
-                      width: 40, // Adjust size as needed
-                      height: 40, // Adjust size as needed
-                      backgroundColor: '#f0f0f0', // Adjust color as needed
+                      width: 40, // Circle size
+                      height: 40, // Circle size
+                      backgroundColor: '#f0f0f0', // Background color
                       borderRadius: '50%',
                       display: 'flex',
                       alignItems: 'center',
                       justifyContent: 'center',
                     }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#808080" className="bi bi-bootstrap-reboot" viewBox="0 0 16 16">
-                      <path d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 1 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.8 6.8 0 0 0 1.16 8z"/>
-                      <path d="M6.641 11.671V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352zm0-3.75V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324z"/>
-                    </svg>
+                      <i className={getClassName(transaction.category?.categoryName)} style={{
+                      fontSize: 33, // Adjust icon size as needed
+                      color: '#808080', // Adjust icon color if needed
+                    }}></i>
                      </div>
                     </div>
                   </div>
