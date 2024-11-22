a4e744d917adfe045e8850fbd556531c62ea5d41
Nic.Ard <nicolas.ard@gmail.com>
2024-11-23 00:49:41 +0100

    fix

--- backend/backend-app/src/main/java/ar/nic/demoproject/services/PrincipalMapperService.java
+++ backend/backend-app/src/main/java/ar/nic/demoproject/services/PrincipalMapperService.java
@@ -5,6 +5,8 @@ import ar.nic.demoproject.db.model.UserProfile;
 import ar.nic.demoproject.db.repository.UserProfileRepository;
 import ar.nic.security.service.CustomJwtTokenUtils;
 import java.security.*;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,25 +33,15 @@ public class PrincipalMapperService {
 
     public Mono<UserProfile> getUserProfile(Principal principal) {
         if (principal instanceof final JwtAuthenticationToken jwtAuthenticationToken) {
-            final String email =
-                    jwtAuthenticationToken.getTokenAttributes().get("email").toString();
-            final String fullName =
-                    jwtAuthenticationToken.getTokenAttributes().get("name").toString();
+            Map<String, Object> tokenAttributes = jwtAuthenticationToken.getTokenAttributes();
+            final String email = String.valueOf(tokenAttributes.get("email"));
+            final String fullName = String.valueOf(tokenAttributes.get("name"));
             final UserProfile userProfile = new UserProfile(fullName, email);
             userProfile.setDefaultCurrency(new Currency("978"));
             return this.userProfileRepository
                     .findByEmail(Mono.just(email))
                     .switchIfEmpty(userProfileRepository.save(userProfile));
         }
-        return Mono.just(new UserProfile("Unknown User", "unknown@email.com"));
-    }
-
-    private Mono<UserProfile> getUserFromDB(UserProfile userProfile) {
-        return userProfileRepository
-                .findByEmail(Mono.just(userProfile.getEmail()))
-                .switchIfEmpty(
-                        userProfileRepository.save(
-                                new UserProfile(
-                                        userProfile.getFullName(), userProfile.getEmail())));
+        throw new RuntimeException("Unable get user profile.");
     }
 }
